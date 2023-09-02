d2f056e5ab5b2b8b0a95e3a06509ea4c7ee56697
Jake Siney <jakesiney@gmail.com>
2023-09-02 11:30:50 +0100

    Init Boostrap

--- website/views.py
+++ website/views.py
@@ -2,19 +2,9 @@ from flask import Blueprint, render_template
 
 # Create a Blueprint object named 'main'. The first argument is the Blueprint's name, which is used by Flask's routing mechanism. The second argument, __name__, is the Blueprint's import name, which Flask uses to locate the Blueprint's resources.
 
-auth = Blueprint('auth', __name__)
+views = Blueprint('views', __name__)
 
 
-@auth.route('/login')
-def login():
-    return "<p>Login</p>"
-
-
-@auth.route('/sign-up')
-def sign_up():
-    return "<p>Sign Up</p>"
-
-
-@auth.route('/logout')
-def logout():
-    return "<p>Logout</p>"
+@views.route('/')
+def index():
+    return render_template('base.html')
