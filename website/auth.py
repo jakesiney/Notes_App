c151e4676c3c5be831060b5ca3097c4b35766503
Jake Siney <jakesiney@gmail.com>
2023-09-03 20:58:04 +0100

    Bug fixing

--- website/auth.py
+++ website/auth.py
@@ -1,12 +1,14 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 # Create a Blueprint object named 'main'. The first argument is the Blueprint's name, which is used by Flask's routing mechanism. The second argument, __name__, is the Blueprint's import name, which Flask uses to locate the Blueprint's resources.
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template("login.html")
 
 
@@ -15,6 +17,29 @@ def logout():
     return "<p>Logout</p>"
 
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        # Get the data from the form
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        if len(email) < 4:
+            flash('Email must be greater than 4 characters.', category='error')
+        elif len(first_name) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        # else:
+        #     # Add user to the database
+        #     new_user = User(email=email, first_name=first_name,
+        #                     password=generate_password_hash(password1, method='sha256'))
+        #     db.session.add(new_user)
+        #     db.session.commit()
+        #     flash('Account created!', category='success')
+        #     return redirect(url_for('views.home'))
+
     return render_template("sign_up.html")
