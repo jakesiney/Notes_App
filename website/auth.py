48880d2ff25a9aa53d88a25e147158f5f603a3fd
Jake Siney <jakesiney@gmail.com>
2023-09-05 20:41:26 +0100

    Import user to templates

--- website/auth.py
+++ website/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
-
+from flask_login import login_user, login_required, logout_user, current_user
 
 # Create a Blueprint object named 'auth'. The first argument is the Blueprint's name, which is used by Flask's routing mechanism. The second argument, __name__, is the Blueprint's import name, which Flask uses to locate the Blueprint's resources.
 auth = Blueprint('auth', __name__)
@@ -18,16 +18,21 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
                 return redirect(url_for('views.index'))
             else:
                 flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
 
-    return render_template("login.html", bool=True)
+    return render_template("login.html", user=current_user)
 
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
@@ -60,7 +65,8 @@ def sign_up():
                             password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
+            login_user(new_user, remember=True)
             flash('Account created!', category='success')
             return redirect(url_for('views.index'))
 
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
--- website/views.py
+++ website/views.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
+
 
 # Create a Blueprint object named 'main'. The first argument is the Blueprint's name, which is used by Flask's routing mechanism. The second argument, __name__, is the Blueprint's import name, which Flask uses to locate the Blueprint's resources.
 
@@ -6,6 +8,7 @@ views = Blueprint('views', __name__)
 
 
 @views.route('/')
+@login_required
 def index():
     # render_template() is a Flask function that takes a template file name as an argument. Flask will look for the template file in the templates folder. Flask will then render the template file as HTML and return it to the client.
-    return render_template('home.html')
+    return render_template('home.html', user=current_user)
